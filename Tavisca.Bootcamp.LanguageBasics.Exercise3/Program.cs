9c16329da78d0686af9c6edc135f5b9d45a648de
tavisca-ksingh <51925469+tavisca-ksingh@users.noreply.github.com>
2019-06-27 16:57:08 +0530

    Update Program.cs

--- Tavisca.Bootcamp.LanguageBasics.Exercise3/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise3/Program.cs
@@ -7,25 +7,26 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
     {
      static void Main(string[] args)
         {
-           Test(
-                new[] { 3, 4 }, 
-                new[] { 2, 8 }, 
-                new[] { 5, 2 }, 
-                new[] { "P", "p","C","c", "F", "f", "T", "t" }, 
-                new[] { 1, 0, 1, 0, 0, 1, 1, 0 });
-           Test(
-                new[] { 3, 4, 1, 5 }, 
-                new[] { 2, 8, 5, 1 }, 
-                new[] { 5, 2, 4, 4 }, 
-                new[] { "tFc", "tF", "Ftc" }, 
-                new[] { 3, 2, 0 });
             Test(
-                new[] { 18, 86, 76, 0, 34, 30, 95, 12, 21 }, 
-                new[] { 26, 56, 3, 45, 88, 0, 10, 27, 53 }, 
-                new[] { 93, 96, 13, 95, 98, 18, 59, 49, 86 }, 
-                new[] { "f", "Pt", "PT", "fT", "Cp", "C", "t", "", "cCp", "ttp", "PCFt", "P", "pCt", "cP", "Pc" }, 
-                new[] { 2, 6, 6, 2, 4, 4, 5, 0, 5, 5, 6, 6, 3, 5, 6 });
-            
+                 new[] { 3, 4 },
+                 new[] { 2, 8 },
+                 new[] { 5, 2 },
+                 new[] { "P", "p", "C", "c", "F", "f", "T", "t" },
+                 new[] { 1, 0, 1, 0, 0, 1, 1, 0 });
+            Test(
+                 new[] { 3, 4, 1, 5 },
+                 new[] { 2, 8, 5, 1 },
+                 new[] { 5, 2, 4, 4 },
+                 new[] { "tFc", "tF", "Ftc" },
+                 new[] { 3, 2, 0 });
+            Test(
+                 new[] { 18, 86, 76, 0, 34, 30, 95, 12, 21 },
+                 new[] { 26, 56, 3, 45, 88, 0, 10, 27, 53 },
+                 new[] { 93, 96, 13, 95, 98, 18, 59, 49, 86 },
+                 new[] { "f", "Pt", "PT", "fT", "Cp", "C", "t", "", "cCp", "ttp", "PCFt", "P", "pCt", "cP", "Pc" },
+                 new[] { 2, 6, 6, 2, 4, 4, 5, 0, 5, 5, 6, 6, 3, 5, 6 });
+
+
         }
 
         private static void Test(int[] protein, int[] carbs, int[] fat, string[] dietPlans, int[] expected)
@@ -40,97 +41,95 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
 
         public static int[] SelectMeals(int[] protein, int[] carbs, int[] fat, string[] dietPlans)
         {
-            // Add your code here.
-            int n = protein.Length;
-            var calories = new int[n];
+            int mealsCount = protein.Length;
+            var calories = new int[mealsCount];
 
-            for(int i = 0;i <n; i++)
+            for (int i = 0; i < mealsCount; i++)
             {
-                calories[i] =( protein[i] + carbs[i])*5 + fat[i]* 9 ;
+                calories[i] = (protein[i] + carbs[i]) * 5 + fat[i] * 9;
             }
-           var meals = new int[dietPlans.Length];
-             for(int i=0; i<dietPlans.Length;i++)
-             {
-                 string dp = dietPlans[i];
-                 
-                if(dp.Length == 0) meals[i]=0;
+            var meals = new int[dietPlans.Length];
+            for (int i = 0; i < dietPlans.Length; i++)
+            {
+                string dietPlan = dietPlans[i];
 
-                List<int> ind = new List<int>();
-                for(int k=0; k<n; k++) ind.Add(k);
-                    int max,min;
-                foreach(var ch in dp)
-                {
-                    switch(ch){
-                        case 'P':
-                            max = MaxNum(protein,ind);
-                            ind = FindAllIndices(protein,ind,max);
-                           break;
-                        case 'p':
-                            min = MinNum(protein,ind);
-                             ind = FindAllIndices(protein,ind,min);
-                            break;
-                         case 'F':
-                            max = MaxNum(fat,ind);
-                            ind = FindAllIndices(fat,ind,max);
-                           break;
-                        case 'f':
-                            min = MinNum(fat,ind);
-                             ind = FindAllIndices(fat,ind,min);
-                            break;
-                         case 'C':
-                            max = MaxNum(carbs,ind);
-                             ind = FindAllIndices(carbs,ind,max);
-                           break;
-                        case 'c':
-                            min = MinNum(carbs,ind);
-                             ind = FindAllIndices(carbs,ind,min);
-                            break;
-                         case 'T':
-                            max = MaxNum(calories,ind);
-                             ind = FindAllIndices(calories,ind,max);
-                           break;
-                        case 't':
-                            min = MinNum(calories,ind);
-                            ind = FindAllIndices(calories,ind,min);
-                            break;
-                        
-                    }
-                }
-                meals[i] = ind[0];
-            }           
+                if (dietPlan.Length == 0) meals[i] = 0;
+
+                var MealsIndex = GetMealsDetails(protein, carbs, fat, calories, dietPlan);
+
+                meals[i] = MealsIndex[0];
+
+            }
             return meals;
         }
 
-        public static List<int> FindAllIndices(int[] arr, List<int> indx,int elem)
+        public static List<int> GetMealsDetails(int[] protein, int[] carbs, int[] fat, int[] calories, string deitplan)
         {
             List<int> indices = new List<int>();
-            foreach(int i in indx){
-                if(arr[i]== elem) indices.Add(i);
+
+            for (int k = 0; k < protein.Length; k++) indices.Add(k);
+
+            foreach (var ch in deitplan)
+            {
+                switch (ch)
+                {
+                    case 'P':
+                        indices = MaxNum(protein, indices);
+                        break;
+                    case 'p':
+                        indices = MinNum(protein, indices);
+                        break;
+                    case 'F':
+                        indices = MaxNum(fat, indices);
+                        break;
+                    case 'f':
+                        indices = MinNum(fat, indices);
+                        break;
+                    case 'C':
+                        indices = MaxNum(carbs, indices);
+                        break;
+                    case 'c':
+                        indices = MinNum(carbs, indices);
+                        break;
+                    case 'T':
+                        indices = MaxNum(calories, indices);
+                        break;
+                    case 't':
+                        indices = MinNum(calories, indices);
+                        break;
+
+                }
             }
             return indices;
         }
-        public static int MaxNum(int [] arr,List<int> indx ){
-                if(indx.Count == 1) return arr[indx[0]];
 
-                int max = arr[indx[0]];
-                
-                for(int i=1; i<indx.Count; i++)
-                {
-                    if(arr[indx[i]] > max) max = arr[indx[i]];
-                }
-                return max;
-            }
-        public static int MinNum(int [] arr,List<int> indx ){
-                if(indx.Count == 1) return arr[indx[0]];
 
-                var min = arr[indx[0]];
-                
-                for(int i=1; i<indx.Count; i++)
-                {
-                    if(arr[indx[i]] < min) min = arr[indx[i]];
-                }
-                return min;
-            }
+        public static List<int> MaxNum(int[] arr, List<int> indx)
+        {
+
+            List<int> Temp1 = new List<int>();
+
+            int max = arr[indx[0]];
+            for (int i = 1; i < indx.Count; i++)
+                if (arr[indx[i]] > max) max = arr[indx[i]];
+
+            for (int i = 0; i < indx.Count; i++)
+                if (arr[indx[i]] == max) Temp1.Add(indx[i]);
+
+            return Temp1;
+        }
+        public static List<int> MinNum(int[] arr, List<int> indx)
+        {
+            List<int> Temp1 = new List<int>();
+            int min = arr[indx[0]];
+            for (int i = 1; i < indx.Count; i++)
+                if (arr[indx[i]] < min) min = arr[indx[i]];
+
+            for (int i = 0; i < indx.Count; i++)
+                if (arr[indx[i]] == min) Temp1.Add(indx[i]);
+
+            return Temp1;
+        }
 
     }
 }
